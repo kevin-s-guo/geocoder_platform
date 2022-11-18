b5455a1924f675e73a22ea57eba2e2442b2e92fa
kevin <kevin.s.guo@vanderbilt.edu>
2022-11-17 22:15:13 -0600

    begin git

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import datetime
+import numpy as np
+import os
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from routers import api, web
+
+app = FastAPI()
+
+app.include_router(web.router)
+app.include_router(api.router)
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
